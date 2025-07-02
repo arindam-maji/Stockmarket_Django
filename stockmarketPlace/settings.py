68e9ed87a238af946e64f75e4656e0f775873d24
arindam-maji <arindam.maji98@gmail.com>
2025-07-02 13:07:51 +0530

    requirements.txt updated

--- stockmarketPlace/settings.py
+++ stockmarketPlace/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 import os
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
