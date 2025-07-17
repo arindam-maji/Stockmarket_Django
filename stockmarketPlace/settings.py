7a6225b51e198dd1111c29ad7e63cc243e024f8e
arindam-maji <arindam.maji98@gmail.com>
2025-07-17 11:15:18 +0530

    done3

--- stockmarketPlace/settings.py
+++ stockmarketPlace/settings.py
@@ -44,12 +44,12 @@ ROOT_URLCONF = 'stockmarketPlace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # If using global templates
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # ✅ Needed
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
